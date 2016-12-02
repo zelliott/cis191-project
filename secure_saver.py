1b32fb5294856dba366474e773c24a6ce6b02bec
Zack Elliott <hubridnoxx@gmail.com>
2016-12-02 15:25:58 -0500

    Added method comments

--- secure_saver.py
+++ secure_saver.py
@@ -3,8 +3,8 @@ import errno
 import json
 
 class SecureSaver:
-  def __init__(self):
 
+  def __init__(self):
     self.path = '.sgcli/'
     self.filename = 'data.json'
     self.emptyData = {
@@ -20,11 +20,13 @@ class SecureSaver:
       json.dump(self.emptyData, f)
       f.close()
 
+  # Saves a field, value to our data file
   def saveField(self, field, value, password):
     data = self.getData(password)
     data[field] = value
     self.saveData(data)
 
+  # Saves data to our data file
   def saveData(self, data):
     if os.path.exists(self.path + self.filename):
       f = open(self.path + self.filename, 'w')
@@ -33,10 +35,12 @@ class SecureSaver:
     else:
       raise Exception('Not found: .sgcli/data.json')
 
+  # Get the value of a field from our data file
   def getField(self, field, password):
     data = self.getData(password)
     return data[field]
 
+  # Get all data from our data file
   def getData(self, password):
     if os.path.exists(self.path + self.filename):
       f = open(self.path + self.filename, 'r')
@@ -47,5 +51,6 @@ class SecureSaver:
     else:
       raise Exception('Not found: .sgcli/data.json')
 
+  # Clear all data from data file
   def clearData(self):
     self.saveData(self.emptyData)
