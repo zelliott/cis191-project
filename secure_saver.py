df43555c7610db3f98baf80b19e7110656dca65a
Zack Elliott <hubridnoxx@gmail.com>
2016-12-02 16:31:18 -0500

    Added security to secure saver

--- secure_saver.py
+++ secure_saver.py
@@ -1,49 +1,86 @@
 import os
 import errno
 import json
+import hashlib, uuid
 
 class SecureSaver:
 
-  def __init__(self):
-    self.path = '.sgcli/'
+  def __init__(self, password=None):
+    self.foldername = '.sgcli/'
     self.filename = 'data.json'
+    self.path = self.foldername + self.filename
     self.emptyData = {
       'email': 'None',
-      'password': 'None'
+      'password': 'None',
+      'salt': 'None'
     }
 
-    if not os.path.exists(self.path):
-      os.makedirs(self.path)
+    if not os.path.exists(self.foldername):
+      os.makedirs(self.foldername)
 
-    if not os.path.exists(self.path + self.filename):
-      f = open(self.path + self.filename, 'w')
+    if not os.path.exists(self.path):
+      f = open(self.path, 'w')
+
+      salt = uuid.uuid4().hex
+      password = hashlib.sha512(password + salt).hexdigest()
+
+      self.emptyData['salt'] = salt
+      self.emptyData['password'] = password
+
       json.dump(self.emptyData, f)
       f.close()
+    else:
 
-  # Saves a field, value to our data file
+      # Important note: If .sgcli/data.json has already been created,
+      # then this new password is effectively ignored by then
+      # SecureSaver.
+      pass
+
+  def confirmPassword(self, password):
+    if os.path.exists(self.path):
+      f = open(self.path, 'r')
+      data = json.load(f)
+
+      savedHashPass = data['password']
+      salt = data['salt']
+      hashPass = hashlib.sha512(password + salt).hexdigest()
+
+      if hashPass != savedHashPass:
+        raise Exception('Incorrect password')
+      else:
+        return True
+
+    else:
+      raise Exception('Not found: .sgcli/data.json')
+
   def saveField(self, field, value, password):
+    self.confirmPassword(password)
+
     data = self.getData(password)
     data[field] = value
-    self.saveData(data)
+    self.saveData(data, password)
 
-  # Saves data to our data file
-  def saveData(self, data):
-    if os.path.exists(self.path + self.filename):
-      f = open(self.path + self.filename, 'w')
+  def saveData(self, data, password):
+    self.confirmPassword(password)
+
+    if os.path.exists(self.path):
+      f = open(self.path, 'w')
       json.dump(data, f)
       f.close()
     else:
       raise Exception('Not found: .sgcli/data.json')
 
-  # Get the value of a field from our data file
   def getField(self, field, password):
+    self.confirmPassword(password)
+
     data = self.getData(password)
     return data[field]
 
-  # Get all data from our data file
   def getData(self, password):
-    if os.path.exists(self.path + self.filename):
-      f = open(self.path + self.filename, 'r')
+    self.confirmPassword(password)
+
+    if os.path.exists(self.path):
+      f = open(self.path, 'r')
       data = json.load(f)
       f.close()
 
@@ -51,6 +88,7 @@ class SecureSaver:
     else:
       raise Exception('Not found: .sgcli/data.json')
 
-  # Clear all data from data file
-  def clearData(self):
+  def clearData(self, password):
+    self.confirmPassword(password)
+
     self.saveData(self.emptyData)
