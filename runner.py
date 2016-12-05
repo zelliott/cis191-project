49afb95b5d10a2da0581c6df549af610538028a1
raghavjoshi <raghav.joshi.15@gmail.com>
2016-12-05 18:33:22 -0500

    Changed to class instead of set of functions. Slowly modifying runner.py to merge two python scripts

--- runner.py
+++ runner.py
@@ -1,49 +1,57 @@
 import sys
+import scrape
+
+order = scrape.Order()
 
 while True:
-  with open('/tmp/sgcli-send', 'r') as send_fifo:
-    data = send_fifo.read().split()
+    with open('/tmp/sgcli-send', 'r') as send_fifo:
+        data = send_fifo.read().split()
 
-    if len(data) == 0:
-      continue;
+        if len(data) == 0:
+            continue;
 
-    flag = data[0]
+        flag = data[0]
 
-    if flag == '--getLocations':
+        if flag == '--getLocations':
 
-      # Params: A zip code
-      # Returns: A list of space-separated locations
-      #   e.g. 'LocationA LocationB LocationC...'
-      zipCode = data[1]
-      toSend = 'LocationA LocationB LocationC'
+            # Params: A zip code
+            # Returns: A list of space-separated locations
+            #   e.g. 'LocationA LocationB LocationC...'
+            zipCode = data[1]
+            order.get_location(zipCode)
+            toSend = ' '.join(order.locations['name'])
 
-    elif flag == '--getMenu':
+        elif flag == '--getMenu':
 
-      # Params: The index of one of the locations in the list
-      # Returns: A list of space-separated menu items
-      #   e.g. 'ItemA ItemB ItemC...'
-      locationNo = data[1]
-      toSend = 'ItemA ItemB ItemC'
+            # Params: The index of one of the locations in the list
+            # Returns: A list of space-separated menu items
+            #   e.g. 'ItemA ItemB ItemC...'
+            locationNo = data[1]
+            rest_id = order.locations['id'][int(locationNo)]
+            menu_items = order.get_menu(rest_id)
+            options = [i.keys()[0] for i in menu_items['menu']]
+            order_options = [i.replace(" ", "_") for i in options]
+            toSend = ' '.join(order_options)
 
-    elif flag == '--getPickupTimes':
+        elif flag == '--getPickupTimes':
 
-      # Params: The index of one of the items in the list
-      # Returns: A list of space-separated pickup times
-      #   e.g. '5:00 5:30 6:00...'
-      itemNo = data[1]
-      toSend = '5:00 5:30 6:00'
+            # Params: The index of one of the items in the list
+            # Returns: A list of space-separated pickup times
+            #   e.g. '5:00 5:30 6:00...'
+            itemNo = data[1]
+            toSend = '5:00 5:30 6:00'
 
-    elif flag == '--confirmOrder':
+        elif flag == '--confirmOrder':
 
-      # Params: The index of one of the times in the list and a password
-      # Returns: `0` upon failure, `1` upon success
-      timeNo = data[1]
-      password = data[2]
-      toSend = '1'
+            # Params: The index of one of the times in the list and a password
+            # Returns: `0` upon failure, `1` upon success
+            timeNo = data[1]
+            password = data[2]
+            toSend = '1'
 
-    else:
-      raise Exeception('Do not recognize api call')
+        else:
+            raise Exception('Do not recognize api call')
 
-    with open('/tmp/sgcli-receive', 'w') as receive_fifo:
-      receive_fifo.write(toSend)
-      receive_fifo.flush()
+        with open('/tmp/sgcli-receive', 'w') as receive_fifo:
+            receive_fifo.write(toSend)
+            receive_fifo.flush()
--- scrape.py
+++ scrape.py
@@ -9,70 +9,82 @@ ORDER_URL = "https://order.sweetgreen.com/api/orders?"
 CHECKOUT_URL = "https://order.sweetgreen.com/api/orders/"
 
 
-def get_location(zip_code):
-    response = {}
-    loc_url = LOCATIONS_URL + str(zip_code)
-    r = requests.get(loc_url).json()
-    restaurants = r['restaurants']
-    response['name'] = [i['restaurant_slug'] for i in restaurants]
-    response['address'] = [i['address'] for i in restaurants]
-    response['id'] = [i['id'] for i in restaurants]
-    return response
+class Order(object):
+    """docstring for Order"""
 
+    def __init__(self):
+        super(Order, self).__init__()
+        self.locations = None
+        self.rest_id = None
+        self.menu = None
+        self.zip = None
 
-def get_menu(rest_id):
-    r = requests.get(MENU_URL + str(rest_id))
-    j = r.json()
-    products = j['products']
-    d = [{i['name']: i['product_slug']} for i in products]
-    return {'menu': d}
+    def get_location(self, zip_code):
+        self.zip = zip_code
+        response = {}
+        loc_url = LOCATIONS_URL + str(self.zip)
+        r = requests.get(loc_url).json()
+        restaurants = r['restaurants']
+        response['name'] = [i['restaurant_slug'] for i in restaurants]
+        response['id'] = [i['id'] for i in restaurants]
+        response['merged'] = [{i['restaurant_slug']: i['id']} for i in restaurants]
+        self.locations = response
+        return self.locations
 
+    def get_menu(self, rest_id):
+        self.rest_id = rest_id
+        r = requests.get(MENU_URL + str(rest_id))
+        j = r.json()
+        products = j['products']
+        d = [{i['name']: i['product_slug']} for i in products]
+        self.menu = {'menu': d}
+        return self.menu
 
-def sign_in(inp):
-    with requests.Session() as s:
-        params = inp.get('customer')
-        pwd = params.get('password')
-        user = params.get('email')
-        payload = 'customer[email]=' + user + '&customer[password]=' + pwd
-        r = s.post(LOGIN_URL, data=payload, headers={"Accept": "application/json",
-                                                     "Content-Type": "application/x-www-form-urlencoded"})
-        resp = r.json()
-        return resp, resp['session']['csrf']
+    def sign_in(self, inp):
+        with requests.Session() as s:
+            params = inp.get('customer')
+            pwd = params.get('password')
+            user = params.get('email')
+            payload = 'customer[email]=' + user + '&customer[password]=' + pwd
+            r = s.post(LOGIN_URL, data=payload, headers={"Accept": "application/json",
+                                                         "Content-Type": "application/x-www-form-urlencoded"})
+            resp = r.json()
+            return resp, resp['session']['csrf']
 
+    def make_order(self, inp, rest_id, prod_id):
+        with requests.session() as s:
+            # Login
+            params = inp.get('customer')
+            pwd = params.get('password')
+            user = params.get('email')
+            payload = 'customer[email]=' + user + '&customer[password]=' + pwd
+            r = s.post(LOGIN_URL, data=payload, headers={"Accept": "application/json",
+                                                         "Content-Type": "application/x-www-form-urlencoded"})
+            resp = r.json()  # to get session information (csrf token)
 
-def make_order(inp, rest_id, prod_id):
-    with requests.session() as s:
-        # Login
-        params = inp.get('customer')
-        pwd = params.get('password')
-        user = params.get('email')
-        payload = 'customer[email]=' + user + '&customer[password]=' + pwd
-        r = s.post(LOGIN_URL, data=payload, headers={"Accept": "application/json",
-                                                     "Content-Type": "application/x-www-form-urlencoded"})
-        resp = r.json() # to get session information (csrf token)
+            # Add line-item
+            payload = 'line_item[quantity]=' + '1' + '&line_item[product_id]=' + str(
+                prod_id) + '&line_item[calories]=' + 'null'
+            payload += '&line_item[favorited]=' + str(False) + '&line_item[is_custom]=' + str(
+                False) + '&line_item[custom_name]=' + 'null'
+            payload += '&line_item[permalink]=' + 'null' + '&line_item[slug]=' + 'null' + '&line_item[static_cost]=' + 'null'
+            payload += '&line_item[additions]=' + 'null' + '&line_item[removals]=' + 'null' + '&line_item[order_completed]=' + 'null'
+            payload += '&line_item[customer_name]=' + 'null' + '&line_item[ignored_order_id]=' + 'null' + '&line_item[order_id]=' + 'null'
+            payload += '&line_item[options]=' + '[]' + '&line_item[restaurant_id]=' + str(rest_id)
+            order = s.post(url=LINE_URL, data=payload, headers={"X-CSRF-TOKEN": resp['session']['csrf']}).json()
 
-        # Add line-item
-        payload = 'line_item[quantity]=' + '1' + '&line_item[product_id]=' + str(prod_id) + '&line_item[calories]=' + 'null'
-        payload += '&line_item[favorited]=' + str(False) + '&line_item[is_custom]=' + str(False) + '&line_item[custom_name]=' + 'null'
-        payload += '&line_item[permalink]=' + 'null' + '&line_item[slug]=' + 'null' + '&line_item[static_cost]=' + 'null'
-        payload += '&line_item[additions]=' + 'null' + '&line_item[removals]=' + 'null' + '&line_item[order_completed]=' + 'null'
-        payload += '&line_item[customer_name]=' + 'null' + '&line_item[ignored_order_id]=' + 'null' + '&line_item[order_id]=' + 'null'
-        payload += '&line_item[options]=' + '[]' + '&line_item[restaurant_id]=' + str(rest_id)
-        order = s.post(url=LINE_URL, data=payload, headers={"X-CSRF-TOKEN":resp['session']['csrf']}).json()
+            # Create order
+            full_url = ORDER_URL + "id=" + str(order['line_item']['ignored_order_id']) + \
+                       "&ignored_product_id=" + str(order['line_item']['product_id'])
 
-        # Create order
-        full_url = ORDER_URL + "id=" + str(order['line_item']['ignored_order_id']) + \
-                   "&ignored_product_id=" + str(order['line_item']['product_id'])
+            # Checkout
+            session = s.get(url="https://order.sweetgreen.com/api/session").json()
+            checkout = s.get(url=CHECKOUT_URL + str(session['session']['current_order_id']),
+                             headers={"X-CSRF-TOKEN": session['session']['csrf']})
 
-        # Checkout
-        session = s.get(url="https://order.sweetgreen.com/api/session").json()
-        checkout = s.get(url=CHECKOUT_URL + str(session['session']['current_order_id']),
-                         headers={"X-CSRF-TOKEN": session['session']['csrf']})
+            # Payment
+            # final_order = s.post()
 
-        # Payment
-        # final_order = s.post()
+            return checkout.json()
 
-        return checkout.json()
-
-
-print make_order({"customer": {"email": "snowboarderwv@hotmail.com", "password": "pwdpwdpwd"}}, 7, 159)
+order = Order()
