e24d72abb20d63f094b50197ee8cdc1d4a6d0d72
raghavjoshi <raghav.joshi.15@gmail.com>
2016-12-05 17:02:29 -0500

    Fixed bug with make order in csrf token

--- scrape.py
+++ scrape.py
@@ -1,12 +1,12 @@
 import requests
-import json
 
 LOCATIONS_URL = "https://order.sweetgreen.com/api/restaurants?zip_code="
 MENU_URL = "https://order.sweetgreen.com/api/menus/"
 LINE_URL = "https://order.sweetgreen.com/api/line_items"
 SIGNUP_URL = "https://order.sweetgreen.com/api/customers/login_or_register"
 LOGIN_URL = "https://order.sweetgreen.com/api/customers/login_or_register"
-ORDER_URL = "https://order.sweetgreen.com/api/orders?id=1512498&ignored_product_id=159"
+ORDER_URL = "https://order.sweetgreen.com/api/orders?"
+CHECKOUT_URL = "https://order.sweetgreen.com/api/orders/"
 
 
 def get_location(zip_code):
@@ -28,34 +28,6 @@ def get_menu(rest_id):
     return {'menu': d}
 
 
-def create_user(first_name, last_name, email, phone_number, pwd):
-    params = {"customer": {
-        "olo_id": None,
-        "email": email,
-        "password": pwd,
-        "password_confirmation": None,
-        "first_name": first_name,
-        "last_name": last_name,
-        "contact_number": phone_number,
-        "reference": None,
-        "is_rewards_user": False,
-        "has_opted_in": True,
-        "reset_password": False,
-        "last_completed_order_id": None,
-        "billing_account_ids": [],
-        "loyalty_id": None
-    }}
-
-    payload = 'customer[olo_id]=' + 'null' + '&customer[email]=' + str(email) + '&customer[password]=' + str(pwd)
-    payload += '&customer[password_confirmation]=' + "null" + '&customer[first_name]=' + str(first_name) + '&customer[last_name]=' + str(last_name)
-    payload += '&customer[contact_number]=' + str(phone_number) + '&customer[reference]=' + 'null' + '&customer[is_rewards_user]=' + str(False)
-    payload += '&customer[has_opted_in]=' + str(True) + '&customer[reset_password]=' + str(False) + '&customer[last_completed_order_id]=' + 'null'
-    payload += '&customer[billing_account_ids]=' + '[]' + '&customer[loyalty_id]=' + 'null'
-
-    r = requests.post(SIGNUP_URL, data=params)
-    return r.json()
-
-
 def sign_in(inp):
     with requests.Session() as s:
         params = inp.get('customer')
@@ -68,7 +40,6 @@ def sign_in(inp):
         return resp, resp['session']['csrf']
 
 
-
 def make_order(inp, rest_id, prod_id):
     with requests.session() as s:
         # Login
@@ -78,24 +49,30 @@ def make_order(inp, rest_id, prod_id):
         payload = 'customer[email]=' + user + '&customer[password]=' + pwd
         r = s.post(LOGIN_URL, data=payload, headers={"Accept": "application/json",
                                                      "Content-Type": "application/x-www-form-urlencoded"})
-        resp = r.json()
-        print resp
+        resp = r.json() # to get session information (csrf token)
 
-        # Make Order
+        # Add line-item
         payload = 'line_item[quantity]=' + '1' + '&line_item[product_id]=' + str(prod_id) + '&line_item[calories]=' + 'null'
         payload += '&line_item[favorited]=' + str(False) + '&line_item[is_custom]=' + str(False) + '&line_item[custom_name]=' + 'null'
         payload += '&line_item[permalink]=' + 'null' + '&line_item[slug]=' + 'null' + '&line_item[static_cost]=' + 'null'
         payload += '&line_item[additions]=' + 'null' + '&line_item[removals]=' + 'null' + '&line_item[order_completed]=' + 'null'
         payload += '&line_item[customer_name]=' + 'null' + '&line_item[ignored_order_id]=' + 'null' + '&line_item[order_id]=' + 'null'
         payload += '&line_item[options]=' + '[]' + '&line_item[restaurant_id]=' + str(rest_id)
-        o = s.post(url=LINE_URL, data=payload, headers={"X-CSRF-TOKEN":resp['session']['csrf']})
-        # o = s.post(url=ORDER_URL, data=payload, headers={"X-CSRF-TOKEN": resp['session']['csrf']})
-        # return o.json()
+        order = s.post(url=LINE_URL, data=payload, headers={"X-CSRF-TOKEN":resp['session']['csrf']}).json()
+
+        # Create order
+        full_url = ORDER_URL + "id=" + str(order['line_item']['ignored_order_id']) + \
+                   "&ignored_product_id=" + str(order['line_item']['product_id'])
+
+        # Checkout
+        session = s.get(url="https://order.sweetgreen.com/api/session").json()
+        checkout = s.get(url=CHECKOUT_URL + str(session['session']['current_order_id']),
+                         headers={"X-CSRF-TOKEN": session['session']['csrf']})
+
+        # Payment
+        # final_order = s.post()
+
+        return checkout.json()
 
 
-# print make_order({"customer": {"email": "snowboarderwv@hotmail.com", "password": "pwdpwdpwd"}}, 7, 159)
-
-# print sign_in({"customer": {"email": "snowboarderwv@hotmail.com", "password": "pwdpwdpwd"}})
-
-
-print create_user("Lemon", "Juice", "lemonjuice@hotmail.com", "8379094434", "welcome")
+print make_order({"customer": {"email": "snowboarderwv@hotmail.com", "password": "pwdpwdpwd"}}, 7, 159)
